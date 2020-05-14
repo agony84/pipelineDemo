ab5dc028fb51e109ebbcdb0e3b666d1887b51bce
agony84 <jjk198478@gmail.com>
2020-05-14 14:47:54 -0600

    adds pipeline model

---
+++ pipelineMVCDemo/Models/Pipeline.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace pipelineMVCDemo.Models{
+
+    public class Pipeline{
+        public int Length { get; set; }
+        public int Width { get; set; }
+        public int Throughput { get; set; }
+    }
+}
